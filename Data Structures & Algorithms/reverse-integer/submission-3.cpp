42bb12d536ec0ddd2ceccf554efa534dd5245431
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-08-09 17:11:29 +0530

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+        bool check=false;
+        if(x<0) check=true;
+        int num=abs(x);
+        int ans=0;
+        while(num){
+            int digit=num%10;
+            if(ans > INT_MAX/10 || ans < INT_MIN/10) return 0;
+            if( (ans==INT_MAX/10 && digit > INT_MAX % 10) || (ans==INT_MIN && digit >  INT_MAX % 10)){
+                return 0;
+            }
+            ans =ans*10+digit;
+            num/=10;
+        }
+        if(check){
+            ans*=-1;
+        }
+        return ans;
+    }
+};
